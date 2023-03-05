854defb72f090a0bb8dae4a041c251bf95eb0005
alexxgru <alexgrundell@hotmail.com>
2023-03-05 21:29:13 +0100

    update

---
+++ script.js
@@ -0,0 +1,44 @@
+
+function animateBorder(element) {
+    return new Promise(resolve => {
+        element.classList.add("animate-border");
+        setTimeout(resolve, 1000);
+    })
+}
+
+async function type(element, text , animate, delay) {
+    return new Promise(resolve => {
+        let i = 0;
+      
+        async function writeChar() {
+          element.textContent += text.charAt(i);
+          if (i < text.length) {
+            i++;
+            setTimeout(writeChar, delay);
+          }
+          else if (animate) {
+            await animateBorder(element);
+            resolve();
+          }
+          else {
+            resolve();
+          }
+        }
+        writeChar();
+    })
+  
+}
+
+async function runAnimation() {
+    let response = await fetch("content.json");
+    let json = await response.json();
+
+    await type(document.querySelector('h1'), json.title , true, 100);
+    await type(document.querySelector('#name'), json.name, false, 75);
+    await type(document.querySelector('#presentation'), json.developer, false, 75);
+    await type(document.querySelector('#lia'), json.lia, false, 75);
+    setTimeout( () => {document.querySelector('#socials').classList.add('visible');}, 1000)
+    document.querySelector('#socials').classList.add('visible');
+}
+
+runAnimation();
